b747302833cabd20eb3525e23baff058973d85d2
Bruno Castellon <Brunocastellon0@gmail.com>
2026-05-27 21:37:02 -0400

    correccion validacion

--- src/test/java/com/swag/pe/steps/validation/ValidationStep.java
+++ src/test/java/com/swag/pe/steps/validation/ValidationStep.java
@@ -2,15 +2,18 @@ package com.swag.pe.steps.validation;
 
 import com.swag.pe.pages.validations.ValidationPage;
 import net.serenitybdd.annotations.Step;
+import java.time.Duration; // Importamos la librería para manejar el tiempo
 
 public class ValidationStep extends ValidationPage {
+    
     @Step("Validar visualizacion del modulo de productos")
     public Boolean titleIsVisible() {
-        return lbl_product.isDisplayed();
+        // Le damos 10 segundos de paciencia al robot para que el elemento aparezca
+        return lbl_product.withTimeoutOf(Duration.ofSeconds(10)).waitUntilVisible().isDisplayed();
     }
+    
     @Step("Validar visualizacion del mensaje de error")
     public Boolean errorMessageIsDisplayed(){
         return lbl_errorMessage.isDisplayed();
     }
-
 }
