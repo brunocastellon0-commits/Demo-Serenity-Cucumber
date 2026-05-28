f9f38a9b7ec60468f5205ed7c52df4aa09d034c4
Bruno Castellon <Brunocastellon0@gmail.com>
2026-05-27 21:41:52 -0400

    ojala

--- src/test/java/com/swag/pe/pages/validations/ValidationPage.java
+++ src/test/java/com/swag/pe/pages/validations/ValidationPage.java
@@ -5,8 +5,11 @@ import net.thucydides.core.pages.PageObject;
 import org.openqa.selenium.support.FindBy;
 
 public class ValidationPage extends PageObject {
-    @FindBy(xpath = "//div[@class='product_label']")
+    
+    // XPath actualizado a la nueva versión de Swag Labs
+    @FindBy(xpath = "//span[@class='title']")
     protected WebElementFacade lbl_product;
+    
     @FindBy(css="h3[data-test='error']")
     protected WebElementFacade lbl_errorMessage;
 
